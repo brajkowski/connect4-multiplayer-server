0e12649bf8f7c56785232fe5915917047d304078
Brandon <brandon.rajkowski@gmail.com>
2021-02-14 15:38:58 -0700

    added session.ts

---
+++ src/session.ts
@@ -0,0 +1,34 @@
+import { BitboardLogic, Player } from '@brajkowski/connect4-web-logic';
+import { Action } from './model/action';
+import { ClientPacket } from './model/client-packet';
+
+export class Session {
+  private logic = new BitboardLogic();
+  private userMap = new Map<string, Player>();
+  private activePlayer = Player.One;
+
+  handlePacket(packet: ClientPacket): void {
+    switch (packet.action) {
+      case Action.JOIN:
+        this.mapUser(packet.user);
+      case Action.MOVE:
+        this.move(packet.user, packet.column);
+    }
+  }
+
+  private move(user: string, column: number) {
+    if (this.logic.canPlaceChip(column)) {
+      this.logic.placeChip(this.userMap.get(user), column);
+    }
+  }
+
+  private mapUser(user: string) {
+    if (this.userMap.size === 0) {
+      this.userMap.set(user, Player.One);
+    } else if (this.userMap.size === 1) {
+      this.userMap.set(user, Player.Two);
+    } else {
+      throw new Error('Too many players joined session');
+    }
+  }
+}
