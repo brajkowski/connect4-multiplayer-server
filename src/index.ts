5fcdef3e5eb2d730af1fb1fc142193e8736c9bf2
Brandon Rajkowski <github@brajkowski.bulc.club>
2021-03-28 12:13:47 -0600

    removed client code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,4 @@
-import { Connect4Client } from './client';
 import { Connect4Server } from './server';
 
-const address = 'ws://localhost:8080';
-const session = 'test-session';
-
 const server = new Connect4Server();
-const owner = new Connect4Client();
-const opponent = new Connect4Client();
 server.start(8080);
-
-owner.open(address);
-opponent.open(address);
-
-setTimeout(() => {
-  owner.createSession(session, 'owner');
-  opponent.joinSession(session, 'opponent');
-}, 1000);
-
-setTimeout(() => {
-  owner.makeMove(0);
-}, 2000);
-
-setTimeout(() => {
-  opponent.makeMove(1);
-}, 3000);
