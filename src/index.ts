d41ab52a046c55252c54fa701b7d7890aef7c591
brajkowski <37646936+brajkowski@users.noreply.github.com>
2021-07-11 10:04:03 -0600

    Added Asyncapi docs (#2)
    * added api.yml with first 3 client pub messages defined
    
    * added quit pub operation
    
    * added opponentJoin sub operation
    
    * added opponentMove sub operation
    
    * added sessionCreated sub operation
    
    * added joinedSession sub operation
    
    * added sessionNotFound sub operation
    
    * added opponentQuit sub operation
    
    * added gameRestart sub operation
    
    * added sessionEnded sub operation
    
    * added @asyncapi/generator dev dependency
    
    * allowing module interop
    
    * server documentation is now generated and served as the index page
    
    * all HTTP routes now redirect to root ('/')
    
    * added service and server description, added contact info
    
    * updated description

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import winston = require('winston');
+import express from 'express';
+import http from 'http';
 import { Connect4Server } from './server';
 
 export const logger = winston.createLogger({
@@ -10,5 +12,21 @@ export const logger = winston.createLogger({
   transports: new winston.transports.Console(),
 });
 
-const server = new Connect4Server();
-server.start(+process.env.PORT || 8081);
+const app = express();
+const httpServer = http.createServer(app);
+const connect4Server = new Connect4Server();
+const port = +(process.env.PORT || '8080');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  if (req.path !== '/') {
+    return res.redirect('/');
+  }
+  next();
+});
+
+httpServer.listen(port, () => {
+  connect4Server.start(httpServer);
+  logger.info(`Server started listening on port: ${port}`);
+});
